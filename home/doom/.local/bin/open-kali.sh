cbc04b28700675eff5a3cf6630d074b267c6241c
doom <admin@shubmehetre.com>
2025-09-01 23:08:21 +0530

    routine

--- home/doom/.local/bin/open-kali.sh
+++ home/doom/.local/bin/open-kali.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 VM="attckervm"
-URI="qemu:///system"
 
-# Check if VM is running
-if virsh --connect "$URI" domstate "$VM" | grep -q running; then
-  # VM already running → just attach
-  virt-viewer --full-screen --connect "$URI" --domain-name "$VM"
+# Check if the VM is already running
+if virsh --connect qemu:///system domstate "$VM" | grep -q running; then
+  # VM is running, just attach console via virt-manager
+  virt-manager --connect qemu:///system --show-domain-console attckervm
 else
-  # VM not running → start then attach
-  virsh --connect "$URI" start "$VM"
-  virt-viewer --full-screen --connect "$URI" --domain-name "$VM"
+  # VM is off → start it, then attach console
+  virsh --connect qemu:///system start "$VM"
+  sleep 3 # small delay to let it boot
+  virt-manager --connect qemu:///system --show-domain-console attckervm
 fi
